5865ef704fc58cb19f0ce986b9013188fc91d745
NikitaMakshev <nikita.makshev@mail.ru>
2021-02-20 15:19:44 +0300

    Task_02 Completed

---
+++ Task02_2/tests/BookTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\BookTest;
+
+use App\Book;
+use PHPUnit\Framework\TestCase;
+
+class BookTest extends TestCase
+{
+    public function testSetName()
+    {
+        $book = new Book();
+        $book->setName("Think and Grow Rich");
+
+        $this->assertEquals("Think and Grow Rich", $book->getName());
+    }
+
+    public function testSetAuthors()
+    {
+        $book = new Book();
+        $book->setAuthors(array("Napoleon Hill"));
+
+        $this->assertEquals(array("Napoleon Hill"), $book->getAuthors());
+    }
+
+    public function testSetPublisher()
+    {
+        $book = new Book();
+        $book->setPublisher("Prime-Euro Sign");
+
+        $this->assertEquals("Prime-Euro Sign", $book->getPublisher());
+    }
+
+    public function testSetYear()
+    {
+        $book = new Book();
+        $book->setYear(1937);
+
+        $this->assertEquals(1937, $book->getYear());
+    }
+}
